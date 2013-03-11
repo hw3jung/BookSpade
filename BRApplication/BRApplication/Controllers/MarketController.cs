ded1682db05bb8f8c5ce01980ae8adc13dd20169
MyName <asma.patel@hotmail.com>
2013-03-10 20:39:58 -0400

    Market Controller

--- BRApplication/BRApplication/Controllers/MarketController.cs
+++ BRApplication/BRApplication/Controllers/MarketController.cs
@@ -19,11 +19,38 @@ namespace BRApplication.Controllers
             MarketPostModel mpm2 = new MarketPostModel("Buy this book, buy it NAO!", false, "MATH 135", "Good", "Johnny Depp", DateTime.Now, "978-3-16-148410-0", "John Jacob JingleHeimer", 150.99);
             MarketPostModel mpm3 = new MarketPostModel("This is a nice lookin title isnt it ;)", false, "STAT 230", "Good", "Jack Johnson", DateTime.Now, "978-3-16-148410-0", "Kiets d a", 0.99);
             MarketPostModel mpm4 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
-            
+            MarketPostModel mpm5 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm6 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm7 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm8 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm9 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm10 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm11 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm12 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm13 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm14 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm15 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm16 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm17 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            MarketPostModel mpm18 = new MarketPostModel("Title #4 ", false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
             mpmLst.Add(mpm1);
             mpmLst.Add(mpm2);
             mpmLst.Add(mpm3);
             mpmLst.Add(mpm4);
+            mpmLst.Add(mpm5);
+            mpmLst.Add(mpm6);
+            mpmLst.Add(mpm7);
+            mpmLst.Add(mpm8);
+            mpmLst.Add(mpm9);
+            mpmLst.Add(mpm10);
+            mpmLst.Add(mpm11);
+            mpmLst.Add(mpm12);
+            mpmLst.Add(mpm13);
+            mpmLst.Add(mpm14);
+            mpmLst.Add(mpm15);
+            mpmLst.Add(mpm16);
+            mpmLst.Add(mpm17);
+            mpmLst.Add(mpm18);
 
             return View(mpmLst); 
         }
