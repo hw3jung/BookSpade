c75553bacadce314a5acacab0ae1aa37b417eeed
Hyun Woo Jung <hw3jung@gmail.com>
2013-06-09 12:37:52 -0700

    FE JQuery Validation & css/js Ref changes
    -Implemented FE validation for adding new textbook input fields with a
    Jquery plugin called Qtip
    -Changed references to multiple css/js files to their minified versions
    (for faster loads)

--- BRApplication/BRApplication/Scripts/Common.js
+++ BRApplication/BRApplication/Scripts/Common.js
@@ -6,3 +6,19 @@ var inputDelay = (function () {
         timer = setTimeout(callback, ms);
     };
 })();
+
+$.validator.addMethod("title", function (value, element) {
+    return /^[A-Za-z0-9:\.\(\)\'\s]+$/i.test(value);
+}, "Title must contain only letters, numbers, spaces, and symbols : . ( ) '");
+
+$.validator.addMethod("author", function (value, element) {
+    return this.optional(element) || /^[A-Za-z,\-\.\'\s]+$/i.test(value);
+}, "Author must contain only letters, spaces, and symbols , - '");
+
+$.validator.addMethod("course", function (value, element) {
+    return /^[A-Za-z]+ ?[0-9]+[A-Za-z]?$/i.test(value);
+}, "Invalid course format");
+
+$.validator.addMethod("isbn", function (value, element) {
+    return /^[0-9-]+[A-Za-z]?$/i.test(value);
+}, "Invalid ISBN format");
